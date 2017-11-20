6f86fdb25a4c4c9c4ce229f7ae561c235747c7b6
Марко Кажић <marko.kazic@zamphyr.com>
2017-11-20 14:21:54 +0100

    feat(UI): Adds an SVG icon and a button color to fit brand

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
     summary: 'Login service for Zamphyr accounts',
-    version: '0.0.7',
+    version: '0.0.8',
     git: 'https://github.com/zamphyr/accounts-zamphyr.git',
     name: 'zamphyr:accounts-zamphyr'
 })
